072d5d6ecb5c134eab4566d03f47558e6a805fe9
Svenne1482 <erik.perers@outlook.com>
2021-11-30 12:18:19 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
+import react, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const apiUrl = 'https://ergast.com/api/f1/'
+
+  const [yearInput, SetYearInput] = useState();
+
+  const apiRequest = async (e, year, round) => {
+    console.log(e);
+    e.preventDefault();
+
+    //do{
+    let request = `${year}/${round}/results.json`
+    let loop = true;
+    await axios.get(apiUrl + request)
+      .then(response => {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+        if(response.data.MRData.RaceTable.Races.length == 0)
+          loop = false;
+      })
+      .catch(error => {
+        loop = false;
+      })
+
+    if (!loop)
+      return;
+    else
+      apiRequest(e, year, round + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={(e) => apiRequest(e, yearInput, 0)}>
+      <input type="text" onChange={(e) => SetYearInput(e.target.value)} />
+    </form>
   );
 }
 
